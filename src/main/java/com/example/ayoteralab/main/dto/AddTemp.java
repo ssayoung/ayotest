a57dac35017daf9e2d3f0404c022bf64a2236244
ssayoung <ssayoung2@gmail.com>
2019-12-21 08:22:46 +0900

    Test Master Commit

---
+++ src/main/java/com/example/ayoteralab/main/dto/AddTemp.java
@@ -0,0 +1,8 @@
+package com.example.ayoteralab.main.dto;
+
+public class AddTemp {
+	private void SYSOUT() {
+		// TODO Auto-generated method stub
+		System.out.println("sdjVHXGSDBWYDAYUVDUIVDV");
+	}
+}	
